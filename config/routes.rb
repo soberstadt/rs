dc650e616fcfc1bc042e0e668a2189910d3e06a3
Weston Platter <westonplatter@gmail.com>
2012-11-18 10:40:27 -0600

    make routes simple. start from what we want to use, not what was there before

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,28 @@
 Rs::Application.routes.draw do
-  
-
   get "dashboard/index"
+  root :to => 'carpools#index'
 
   # report controller convers rs1 "email" functionality
-  get "report/all"  
-  match 'carpool/report' => 'carpools#report'
-  match 'carpool/email' => 'carpools#email'
-  match 'carpool/email_submit' => 'carpools#email_submit'
+  # get "report/all"  
+  # match 'carpool/report' => 'carpools#report'
+  # match 'carpool/email' => 'carpools#email'
+  # match 'carpool/email_submit' => 'carpools#email_submit'
 
 
   # shows an individual carpool group (IE, user logins and sees their carpool)
-  get "report/carpool"
+  # get "report/carpool"
   
   # returns list of people not registered for Rideshare
-  get "report/non_participants"
+  # get "report/non_participants"
 
 
   # rs1 routes
-  match 'carpool/login/:id' => 'carpools#login'
-  match 'carpool/get_coordinates' => 'carpools#get_coordinates'
-  match 'carpool/register' => 'carpools#register'
-  match 'carpool/update_addresses' => 'carpools#update_addresses'
-  match 'carpool/:id' => 'carpools#index'
+  # match 'carpool/login/:id' => 'carpools#login'
+  # match 'carpool/get_coordinates' => 'carpools#get_coordinates'
+  # match 'carpool/register' => 'carpools#register'
+  # match 'carpool/update_addresses' => 'carpools#update_addresses'
+  # match 'carpool/:id' => 'carpools#index'
   
   # match 'carpool' => 'carpools#index'
-  
-  root :to => 'carpools#index'
-
 end
 
