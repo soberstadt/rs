7d9c2d3dc611a918df11f67e2c729228af270040
Josh Starcher <josh.starcher@gmail.com>
2013-01-11 10:32:02 -0500

    Update rails

---
+++ vendor/gems/airbrake-3.1.1/lib/airbrake/rack.rb
@@ -0,0 +1,45 @@
+module Airbrake
+  # Middleware for Rack applications. Any errors raised by the upstream
+  # application will be delivered to Airbrake and re-raised.
+  #
+  # Synopsis:
+  #
+  #   require 'rack'
+  #   require 'airbrake'
+  #
+  #   Airbrake.configure do |config|
+  #     config.api_key = 'my_api_key'
+  #   end
+  #
+  #   app = Rack::Builder.app do
+  #     run lambda { |env| raise "Rack down" }
+  #   end
+  #
+  #   use Airbrake::Rack
+  #   run app
+  #
+  # Use a standard Airbrake.configure call to configure your api key.
+  class Rack
+    def initialize(app)
+      @app = app
+      Airbrake.configuration.logger ||= Logger.new STDOUT
+    end
+
+    def call(env)
+      begin
+        response = @app.call(env)
+      rescue Exception => raised
+        error_id = Airbrake.notify_or_ignore(raised, :rack_env => env)
+        env['airbrake.error_id'] = error_id
+        raise
+      end
+
+      if env['rack.exception']
+        error_id = Airbrake.notify_or_ignore(env['rack.exception'], :rack_env => env)
+        env['airbrake.error_id'] = error_id
+      end
+
+      response
+    end
+  end
+end
---
+++ vendor/gems/airbrake-3.1.1/lib/airbrake/version.rb
@@ -0,0 +1,3 @@
+module Airbrake
+  VERSION = "3.1.1".freeze
+end
