600e61a078de37cdae5844905c5ed7bdb14baff5
Spencer Oberstadt <soberstadt@gmail.com>
2013-10-12 13:05:22 -0500

    fix routes to point to carpools controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ Rs::Application.routes.draw do
   get 'carpool/get_coordinates'   => 'carpools#get_coordinates'
   get 'carpool/report'            => 'carpools#report'
   get 'carpool/register/:id'      => 'carpools#register'
-  post 'carpool/register'         => 'carpool#register', as: 'register_submit'
+  post 'carpool/register'         => 'carpools#register', as: 'register_submit'
   get 'carpool/update_addresses'  => 'carpools#update_addresses', via: [:get, :update]
   get 'carpool/:id/empty'         => 'carpools#empty'
   get 'carpool/:id'               => 'carpools#index'
   get 'carpool'                   => 'carpools#index'
   
-  root :to => 'carpool#index'
+  root :to => 'carpools#index'
 
 end
 
